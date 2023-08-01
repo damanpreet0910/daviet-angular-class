9d5c0a83b0ee9b41a1dfe9404841900c33a9ec38
damanpreet kaur <o7services003@gmail.com>
2023-08-01 14:46:15 +0530

    Direct components and routing, Error Component

--- project-1/src/app/app-routing.module.ts
+++ project-1/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',redirectTo:'/first-page',pathMatch:'full'
+  },
+  {
+    path:'first-page',component:HomeComponent
+  },
+  {
+    path:'about-us',component:AboutComponent
+  },
+  {
+    path:'contact-us',component:ContactComponent
+  },
+  {
+    path:'**',component:PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
