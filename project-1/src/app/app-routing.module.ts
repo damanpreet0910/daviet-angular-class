792cef6123a0630a6de8e831968118ca71c19ff9
damanpreet kaur <o7services003@gmail.com>
2023-08-02 14:37:42 +0530

    nested components and nested routing

--- project-1/src/app/app-routing.module.ts
+++ project-1/src/app/app-routing.module.ts
@@ -4,19 +4,33 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AddCategoryComponent } from './category/add-category/add-category.component';
+import { ManageCategoryComponent } from './category/manage-category/manage-category.component';
 
 const routes: Routes = [
   {
-    path:'',redirectTo:'/first-page',pathMatch:'full'
+    path:'',redirectTo:'/layout/first-page',pathMatch:'full'
   },
   {
-    path:'first-page',component:HomeComponent
-  },
-  {
-    path:'about-us',component:AboutComponent
-  },
-  {
-    path:'contact-us',component:ContactComponent
+    path:'layout',component:LayoutComponent,
+    children:[
+      {
+        path:'first-page',component:HomeComponent
+      },
+      {
+        path:'about-us',component:AboutComponent
+      },
+      {
+        path:'contact-us',component:ContactComponent
+      },
+      {
+        path:'add-category',component:AddCategoryComponent
+      },
+      {
+        path:'manage-category',component:ManageCategoryComponent
+      }
+    ]    
   },
   {
     path:'**',component:PageNotFoundComponent
--- project-1/src/app/app.module.ts
+++ project-1/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AddCategoryComponent } from './category/add-category/add-category.component';
+import { ManageCategoryComponent } from './category/manage-category/manage-category.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    AddCategoryComponent,
+    ManageCategoryComponent
   ],
   imports: [
     BrowserModule,
