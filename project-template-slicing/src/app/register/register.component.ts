8c7cec7670b5a73944f120f65500f8ad56e1ad29
damanpreet kaur <o7services003@gmail.com>
2023-08-10 14:44:25 +0530

    template driven form using register

--- project-template-slicing/src/app/register/register.component.ts
+++ project-template-slicing/src/app/register/register.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
 
+  registerform = {
+    name : '',
+    email:'',
+    password:''
+  }
+
+  constructor(private router : Router){}
+
+  ngOnInit(): void {
+    
+  }
+
+  reg(){
+    // console.log(this.registerform)
+    this.router.navigateByUrl("/layout/login")
+  }
 }
